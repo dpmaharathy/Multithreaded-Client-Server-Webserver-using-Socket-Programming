94b71ed049b47ec5b0024fd175313bc87c677b61
Devi Prasad Maharathy <96619281+dpmaharathy@users.noreply.github.com>
2022-07-01 15:41:29 +0530

    Some changes in server program

--- server.py
+++ server.py
@@ -1,53 +1,28 @@
-import socket
-import threading
 import requests
-import pickle
+import os
+from datetime import datetime
 
-HEADER=64
-FORMAT='utf-8'
-PORT=9879
-DISCONNECT_MESSAGE="EXIT"
-SERVER="127.0.0.1"
-#SERVER=socket.gethostbyname(socket.gethostname())
+user_api = "ee938cc3477bf4e627bb49eba464e06f"
 
-ADDR=(SERVER,PORT)
-server=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-server.bind(ADDR)#binding the socket
+location = input("Enter the city name: ")
 
-def handle_client(conn,addr):
-    print("NEW connection ",addr," connected")
+complete_api_link = "https://api.openweathermap.org/data/2.5/weather?q="+location+"&appid="+user_api
+api_link = requests.get(complete_api_link)
+api_data = api_link.json()
+type(api_data)
+#create variables to store and display data
+temp_city = ((api_data['main']['temp']) - 273.15)
+weather_desc = api_data['weather'][0]['description']
+hmdt = api_data['main']['humidity']
+wind_spd = api_data['wind']['speed']
+date_time = datetime.now().strftime("%d %b %Y | %I:%M:%S %p")
 
-    connected=True
-    while connected:
-        msg_len=conn.recv(HEADER).decode(FORMAT)
-        if msg_len:
-            msg_len=int(msg_len)
-            msg=conn.recv(msg_len).decode(FORMAT)
-            if(msg==DISCONNECT_MESSAGE):
-                connected=False
-            print(f"[{addr}] {msg}")
+print ("-------------------------------------------------------------")
+print ("Weather Stats for - {}  || {}".format(location.upper(), date_time))
+print ("-------------------------------------------------------------")
 
-            user_api = "ee938cc3477bf4e627bb49eba464e06f"
-
-            location = input("Enter the city name: ")
-
-            complete_api_link = "https://api.openweathermap.org/data/2.5/weather?q="+location+"&appid="+user_api
-            api_link = requests.get(complete_api_link)
-            api_data = api_link.json()
-            send_msg=pickle.dumps(api_data)
-            conn.send(send_msg)
-    
-    conn.close()
-        
-        
-
-def start():
-    server.listen()
-    print(f"Server is listening on {SERVER}")
-    while True:
-        conn,addr=server.accept()
-        thread=threading.Thread(target=handle_client,args=(conn,addr))
-        thread.start()
-        print(f"ACTIVE connections: {threading.activeCount()-1} :")
-print("Server is starting...")
-start()
+print ("Current temperature is: {:.2f} deg C".format(temp_city))
+print ("Current weather desc  :",weather_desc)
+print ("Current Humidity      :",hmdt, '%')
+print ("Current wind speed    :",wind_spd ,'kmph')
+print ("-------------------------------------------------------------")
